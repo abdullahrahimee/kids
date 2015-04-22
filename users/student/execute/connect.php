501d63382d8c968ec1b57c280da2a9b9d659c00b
Abdullah Rahimee <abdullah_rahemee20@yahoo.com>
2015-04-22 12:50:05 +0430

    keep this change

--- users/student/execute/connect.php
+++ users/student/execute/connect.php
@@ -1,8 +1,8 @@
 <!-- create 2nd -->
 <?php
 	
-	$link = mysql_connect('localhost','root','');
-	$db= mysql_select_db('techkids');
+	$link = mysql_connect('localhost','root','root');
+	$db= mysql_select_db('newkids');
 
 	if (!$link) {
 	die ('Failed to connect to server: ' . mysql_error () );
